273bff9e57d687a895248cf8799d8daceebb0b1d
Steven H. Ranney <steven.ranney@gmail.com>
2018-01-10 23:07:29 -0700

    More cleanup and adding annotations.

--- code/comparingWae3QLines.r
+++ code/comparingWae3QLines.r
@@ -14,14 +14,11 @@ source("code/helper_functions.R")
 # For repeatability
 set.seed(256)
 
-#read in and manipulate the WAE dataset from Ranney et al. 2010 and 2011; data
+# Read in and manipulate the WAE dataset from Ranney et al. 2010 and 2011; data
 # filtering protocols have already been applied
 wae <- read.table("code/WAE Clean.txt", header=T)
-attach(wae)
-names(wae)
 
-
-#Assigns fish to a length category (Gabelhouse 1984)
+# Assigns fish to a length category (Gabelhouse 1984)
 wae <- 
   wae %>%
   mutate(psd = ifelse((length >= 250) & (length < 380), "S-Q",
@@ -30,14 +27,14 @@ wae <-
                                     ifelse((length >= 630) & (length < 760), "M-T",
                                            ifelse(length >= 760, ">T", "SS"))))))
 
-#Assign fish to 10mm length class at midpoint
+# Assign fish to 10mm length class at midpoint
 wae <- 
   wae %>%
   mutate(l.c = lencat(length, w = 10) +5)
 
 
-#Build nonlinear quantile regression models; 
-#USED ONLY FOR CALCULATING A TABLE OF PREDICTED QUANTILE VALUES
+# Build nonlinear quantile regression models;
+# USED ONLY FOR CALCULATING A TABLE OF PREDICTED QUANTILE VALUES
 wae.mod.75 <- nlrq(weight~alpha*length^beta, data=wae, tau=0.75,                         
                    start=list(alpha=0.00001, beta=3))
 wae.mod.5 <- nlrq(weight~alpha*length^beta, data=wae, tau=0.5,                           
@@ -46,22 +43,10 @@ wae.mod.25 <- nlrq(weight~alpha*length^beta, data=wae, tau=0.25,
                    start=list(alpha=0.00001, beta=3))
 
 
-#Build linear quantile regression models
+# Build linear quantile regression models
 wae.linMod.75 <- rq(log10(weight)~log10(length), data=wae, tau=0.75)
 wae.linMod.5 <- rq(log10(weight)~log10(length), data=wae, tau=0.5)
 wae.linMod.25 <- rq(log10(weight)~log10(length), data=wae, tau=0.25)
-
-
-
-#Create a vector of lengths to include in plots
-x <- seq(165,735, by=10)
-
-
-
-
-
-
-
 
 # Create a data.frame of predicted values from the non-linear quantile regression models above
 wae.pred.values <- 
@@ -72,15 +57,15 @@ wae.pred.values <-
          "50th" = `X50th`, 
          "75th" = `X75th`)
 
-
-
+wae.pred.values %>%
+  write.csv(paste0("output/", Sys.Date(), "_wae.pred.values.csv"))
 
 #-------------------------------------------------------------------------------
-#Read in independent data
+# Read in independent data
 
 waeInd <- read.table("code/WAE_independent.txt", header=T)
 
-#Assigns fish to a length category (Gabelhouse 1984)
+# Assigns fish to a length category (Gabelhouse 1984)
 waeInd <- 
   waeInd %>%
   mutate(psd = ifelse((length>=250)&(length<380), "S-Q",
@@ -89,7 +74,7 @@ waeInd <-
                                     ifelse((length>=630)&(length<760), "M-T",
                                            ifelse(length>=760, ">T", "SS"))))))
 
-#Define GA and SD populations
+# Define GA and SD populations
 waeGA <- 
   waeInd %>% 
   filter(State == "GA")
@@ -98,27 +83,24 @@ waeSD <-
   waeInd %>%
   filter(State == "SD")
 
-#From the plots above, GA lakes 2, 3, and 4 have pops that may have Q3 higher 
-#than or equal to the ref 
+# Random GA populations
 ga2 <- rq(log10(weight)~log10(length), data=waeGA[waeGA$lake == "2",], tau=0.75)
 ga3 <- rq(log10(weight)~log10(length), data=waeGA[waeGA$lake == "3",], tau=0.75)
 ga4 <- rq(log10(weight)~log10(length), data=waeGA[waeGA$lake == "4",], tau=0.75)
 
 
-#From the plots above, SD lakes 4, 13, and 25 have pops that may have Q3 higher 
-#than or equal to the ref 
+# Random SD populations
 sd4 <- rq(log10(weight)~log10(length), data=waeSD[waeSD$lake == "4",], tau=0.75)
 sd13 <- rq(log10(weight)~log10(length), data=waeSD[waeSD$lake == "13",], tau=0.75)
 sd25 <- rq(log10(weight)~log10(length), data=waeSD[waeSD$lake == "25",], tau=0.75)
 
 
-
-
 #-----------------------------------------------------------------------------
 
-#Calculate the Blom estimator for Q3 values for all length categories
+# Calculate the Blom estimator for Q3 values for all length categories
 # BlomEstimator fx comes from helper_functions.R
 
+# Reference population
 sSRef <- BlomEstimator(wae$weight[wae$psd=="SS"], c(.25, .5,.75))
 sQRef <- BlomEstimator(wae$weight[wae$psd=="S-Q"], c(.25, .5,.75))
 qPRef <- BlomEstimator(wae$weight[wae$psd=="Q-P"], c(.25, .5,.75))
@@ -126,7 +108,7 @@ pMRef <- BlomEstimator(wae$weight[wae$psd=="P-M"], c(.25, .5,.75))
 mTRef <- BlomEstimator(wae$weight[wae$psd=="M-T"], c(.25, .5,.75))
 gTRef <- c(NA, NA, NA)
 
-#GA lake 2
+# GA 1
 sSGa2 <- BlomEstimator(waeGA$weight[waeGA$psd=="SS" & waeGA$lake == "2"], c(.25, .5, .75))
 sQGa2 <- BlomEstimator(waeGA$weight[waeGA$psd=="S-Q" & waeGA$lake == "2"], c(.25, .5, .75))
 qPGa2 <- BlomEstimator(waeGA$weight[waeGA$psd=="Q-P" & waeGA$lake == "2"], c(.25, .5, .75))
@@ -134,7 +116,7 @@ pMGa2 <- BlomEstimator(waeGA$weight[waeGA$psd=="P-M" & waeGA$lake == "2"], c(.25
 mTGa2 <- BlomEstimator(waeGA$weight[waeGA$psd=="M-T" & waeGA$lake == "2"], c(.25, .5, .75))
 gTGa2 <- c(NA, NA, NA)
 
-#GA lake 3
+# GA 2
 sSGa3 <- BlomEstimator(waeGA$weight[waeGA$psd=="SS" & waeGA$lake == "3"], c(.25, .5, .75))
 sQGa3 <- BlomEstimator(waeGA$weight[waeGA$psd=="S-Q" & waeGA$lake == "3"], c(.25, .5, .75))
 qPGa3 <- BlomEstimator(waeGA$weight[waeGA$psd=="Q-P" & waeGA$lake == "3"], c(.25, .5, .75))
@@ -142,7 +124,7 @@ pMGa3 <- BlomEstimator(waeGA$weight[waeGA$psd=="P-M" & waeGA$lake == "3"], c(.25
 mTGa3 <- BlomEstimator(waeGA$weight[waeGA$psd=="M-T" & waeGA$lake == "3"], c(.25, .5, .75))
 gTGa3 <- c(NA, NA, NA)
 
-#GA lake 4
+# GA 3
 sSGa4 <- BlomEstimator(waeGA$weight[waeGA$psd=="SS" & waeGA$lake == "4"], c(.25, .5, .75))
 sQGa4 <- BlomEstimator(waeGA$weight[waeGA$psd=="S-Q" & waeGA$lake == "4"], c(.25, .5, .75))
 qPGa4 <- BlomEstimator(waeGA$weight[waeGA$psd=="Q-P" & waeGA$lake == "4"], c(.25, .5, .75))
@@ -150,7 +132,7 @@ pMGa4 <- BlomEstimator(waeGA$weight[waeGA$psd=="P-M" & waeGA$lake == "4"], c(.25
 mTGa4 <- BlomEstimator(waeGA$weight[waeGA$psd=="M-T" & waeGA$lake == "4"], c(.25, .5, .75))
 gTGa4 <- c(NA, NA, NA)
 
-#SD lake 4
+# SD 1
 sSSd4 <- BlomEstimator(waeSD$weight[waeSD$psd=="SS" & waeSD$lake == "4"], c(.25, .5, .75))
 sQSd4 <- BlomEstimator(waeSD$weight[waeSD$psd=="S-Q" & waeSD$lake == "4"], c(.25, .5, .75))
 qPSd4 <- BlomEstimator(waeSD$weight[waeSD$psd=="Q-P" & waeSD$lake == "4"], c(.25, .5, .75))
@@ -158,7 +140,7 @@ pMSd4 <- BlomEstimator(waeSD$weight[waeSD$psd=="P-M" & waeSD$lake == "4"], c(.25
 mTSd4 <- BlomEstimator(waeSD$weight[waeSD$psd=="M-T" & waeSD$lake == "4"], c(.25, .5, .75))
 gTSd4 <- c(NA, NA, NA)
 
-#SD lake 13
+# SD 2
 sSSd13 <- BlomEstimator(waeSD$weight[waeSD$psd=="SS" & waeSD$lake == "13"], c(.25, .5, .75))
 sQSd13 <- BlomEstimator(waeSD$weight[waeSD$psd=="S-Q" & waeSD$lake == "13"], c(.25, .5, .75))
 qPSd13 <- BlomEstimator(waeSD$weight[waeSD$psd=="Q-P" & waeSD$lake == "13"], c(.25, .5, .75))
@@ -166,7 +148,7 @@ pMSd13 <- BlomEstimator(waeSD$weight[waeSD$psd=="P-M" & waeSD$lake == "13"], c(.
 mTSd13 <- BlomEstimator(waeSD$weight[waeSD$psd=="M-T" & waeSD$lake == "13"], c(.25, .5, .75))
 gTSd13 <- c(NA, NA, NA)
 
-#SD lake 25
+# SD 3
 sSSd25 <- BlomEstimator(waeSD$weight[waeSD$psd=="SS" & waeSD$lake == "25"], c(.25, .5, .75))
 sQSd25 <- BlomEstimator(waeSD$weight[waeSD$psd=="S-Q" & waeSD$lake == "25"], c(.25, .5, .75))
 qPSd25 <- BlomEstimator(waeSD$weight[waeSD$psd=="Q-P" & waeSD$lake == "25"], c(.25, .5, .75))
@@ -194,65 +176,62 @@ quarTable <- matrix(c(sSRef, sQRef, qPRef, pMRef, mTRef, gTRef,
                                                        ".25",".5", ".75")),
                       byrow=T)
 
-#Write the quarTable to the directory
+# Write the quarTable to the output directory
 quarTable %>%
   write.csv("output/quarTable.csv", row.names=T)
 
 #-----------------------------------------------------------------------------
-#Estimate differences between Reference slope and intercept and other models
+# Estimate differences between Reference slope and intercept and other models
 
-#Bootstrap the sampling distributions of slope and intercept values by 
-#running n regressions with numSamples drawn randomly with replacement from 
-#a given dataset
+# Bootstrap the sampling distributions of slope and intercept values by 
+# running n regressions with numSamples drawn randomly with replacement from 
+# a given dataset
 # EstLinQuantRegCoefDist fx comes from the helper_functions.R file
 
 #Mann-Whitney U test to evaluate for differences between 
 refDist <- EstLinQuantRegCoefDist(wae, 10000, 100, tau=0.75)
 
-ga2Dist <- EstLinQuantRegCoefDist(waeGA[waeGA$lake == "2",], 10000, 100, 0.75)
+ga2Dist <- EstLinQuantRegCoefDist(waeGA %>% filter(lake == "2"), 10000, 100, 0.75)
 
-ga3Dist <- EstLinQuantRegCoefDist(waeGA[waeGA$lake == "3",], 10000, 100, 0.75)
+ga3Dist <- EstLinQuantRegCoefDist(waeGA %>% filter(lake == "3"), 10000, 100, 0.75)
 
-ga4Dist <- EstLinQuantRegCoefDist(waeGA[waeGA$lake == "4",], 10000, 100, 0.75)
+ga4Dist <- EstLinQuantRegCoefDist(waeGA %>% filter(lake == "4"), 10000, 100, 0.75)
 
-sd4Dist <- EstLinQuantRegCoefDist(waeSD[waeSD$lake == "4",], 10000, 100, 0.75)
+sd4Dist <- EstLinQuantRegCoefDist(waeSD %>% filter(lake == "4"), 10000, 100, 0.75)
 
-sd13Dist <- EstLinQuantRegCoefDist(waeSD[waeSD$lake == "13",], 10000, 100, 0.75)
+sd13Dist <- EstLinQuantRegCoefDist(waeSD %>% filter(lake == "13"), 10000, 100, 0.75)
 
-sd25Dist <- EstLinQuantRegCoefDist(waeSD[waeSD$lake == "25",], 10000, 100, 0.75)
+sd25Dist <- EstLinQuantRegCoefDist(waeSD %>% filter(lake == "25"), 10000, 100, 0.75)
 
-#Create a table of slope coefficients and bootstrapped confidence intervals
+# Create a table of slope coefficients and bootstrapped confidence intervals
 slopeVals <- as.data.frame(matrix(data = NA, ncol=3, nrow=7, byrow=T, 
                            dimnames = list(c("Reference", "GA 1", 
                                              "GA 2", "GA 3", "SD 1",
                                              "SD 2", "SD 3"), 
                                            c("2.5", "mean", "97.5"))))
-                                           
-slopeVals$mean <- 
-  c(mean(refDist[,2]), mean(ga2Dist[,2]), mean(ga3Dist[,2]), 
-    mean(ga4Dist[,2]), mean(sd4Dist[,2]), mean(sd13Dist[,2]), 
-    mean(sd25Dist[,2]))
-slopeVals$`2.5` <- 
-  c(BlomEstimator(refDist[,2], 0.025), 
-    BlomEstimator(ga2Dist[,2], 0.025),
-    BlomEstimator(ga3Dist[,2], 0.025),
-    BlomEstimator(ga4Dist[,2], 0.025),
-    BlomEstimator(sd4Dist[,2], 0.025),
-    BlomEstimator(sd13Dist[,2], 0.025),
-    BlomEstimator(sd25Dist[,2], 0.025))
-slopeVals$`97.5` <- 
-  c(BlomEstimator(refDist[,2], 0.975), 
-    BlomEstimator(ga2Dist[,2], 0.975),
-    BlomEstimator(ga3Dist[,2], 0.975),
-    BlomEstimator(ga4Dist[,2], 0.975),
-    BlomEstimator(sd4Dist[,2], 0.975),
-    BlomEstimator(sd13Dist[,2], 0.975),
-    BlomEstimator(sd25Dist[,2], 0.975))
 
 slopeVals <- 
-  slopeVals %>% 
-  mutate(pop = row.names(slopeVals))
-               
+  data.frame(`2.5` = c(BlomEstimator(refDist[,2], 0.025), 
+                       BlomEstimator(ga2Dist[,2], 0.025),
+                       BlomEstimator(ga3Dist[,2], 0.025),
+                       BlomEstimator(ga4Dist[,2], 0.025),
+                       BlomEstimator(sd4Dist[,2], 0.025),
+                       BlomEstimator(sd13Dist[,2], 0.025),
+                       BlomEstimator(sd25Dist[,2], 0.025)), 
+             mean = c(mean(refDist[,2]), mean(ga2Dist[,2]), mean(ga3Dist[,2]), 
+                      mean(ga4Dist[,2]), mean(sd4Dist[,2]), mean(sd13Dist[,2]), 
+                      mean(sd25Dist[,2])), 
+             `97.5` = c(BlomEstimator(refDist[,2], 0.975), 
+                        BlomEstimator(ga2Dist[,2], 0.975),
+                        BlomEstimator(ga3Dist[,2], 0.975),
+                        BlomEstimator(ga4Dist[,2], 0.975),
+                        BlomEstimator(sd4Dist[,2], 0.975),
+                        BlomEstimator(sd13Dist[,2], 0.975),
+                        BlomEstimator(sd25Dist[,2], 0.975))) %>%
+  mutate(pop = c("Reference", "GA 1", "GA 2", "GA 3", "SD 1", "SD 2", "SD 3")) %>%
+  rename("2.5" = "X2.5", 
+         "97.5" = "X97.5")
+
 slopeVals %>%
   ggplot(aes(x = pop, y = mean)) +
   geom_point() +
@@ -261,42 +240,36 @@ slopeVals %>%
   geom_hline(yintercept = slopeVals %>% filter(pop == "Reference") %>% .$`2.5`, 
              linetype = 2, size = 0.1) +
   geom_hline(yintercept = slopeVals %>% filter(pop == "Reference") %>% .$`97.5`, 
-             linetype = 2, size = 0.1)
+             linetype = 2, size = 0.1) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))
 
                
 
-#Create a table of intercept coefficients and bootstrapped confidence intervals
-intVals <- as.data.frame(matrix(data = NA, ncol=3, nrow=7, byrow=T, 
-                           dimnames = list(c("Reference", "GA 1", 
-                                             "GA 2", "GA 3", "SD 1",
-                                             "SD 2", "SD 3"), 
-                                           c("2.5", "mean", "97.5"))))
-                                           
-intVals$mean <- 
-  c(mean(refDist[,1]), mean(ga2Dist[,1]), mean(ga3Dist[,1]), 
-    mean(ga4Dist[,1]), mean(sd4Dist[,1]), mean(sd13Dist[,1]), 
-    mean(sd25Dist[,1]))
-intVals$`2.5` <- 
-  c(BlomEstimator(refDist[,1], 0.025), 
-    BlomEstimator(ga2Dist[,1], 0.025),
-    BlomEstimator(ga3Dist[,1], 0.025),
-    BlomEstimator(ga4Dist[,1], 0.025),
-    BlomEstimator(sd4Dist[,1], 0.025),
-    BlomEstimator(sd13Dist[,1], 0.025),
-    BlomEstimator(sd25Dist[,1], 0.025))
-intVals$`97.5` <- 
-  c(BlomEstimator(refDist[,1], 0.975), 
-    BlomEstimator(ga2Dist[,1], 0.975),
-    BlomEstimator(ga3Dist[,1], 0.975),
-    BlomEstimator(ga4Dist[,1], 0.975),
-    BlomEstimator(sd4Dist[,1], 0.975),
-    BlomEstimator(sd13Dist[,1], 0.975),
-    BlomEstimator(sd25Dist[,1], 0.975))
+# Create a table of intercept coefficients and bootstrapped confidence intervals
 
 intVals <- 
-  intVals %>%
-  mutate(pop = row.names(intVals))
-               
+  data.frame(`2.5` = c(BlomEstimator(refDist[,1], 0.025), 
+                       BlomEstimator(ga2Dist[,1], 0.025),
+                       BlomEstimator(ga3Dist[,1], 0.025),
+                       BlomEstimator(ga4Dist[,1], 0.025),
+                       BlomEstimator(sd4Dist[,1], 0.025),
+                       BlomEstimator(sd13Dist[,1], 0.025),
+                       BlomEstimator(sd25Dist[,1], 0.025)), 
+             mean = c(mean(refDist[,1]), mean(ga2Dist[,1]), mean(ga3Dist[,1]), 
+                      mean(ga4Dist[,1]), mean(sd4Dist[,1]), mean(sd13Dist[,1]), 
+                      mean(sd25Dist[,1])), 
+             `97.5` = c(BlomEstimator(refDist[,1], 0.975), 
+                        BlomEstimator(ga2Dist[,1], 0.975),
+                        BlomEstimator(ga3Dist[,1], 0.975),
+                        BlomEstimator(ga4Dist[,1], 0.975),
+                        BlomEstimator(sd4Dist[,1], 0.975),
+                        BlomEstimator(sd13Dist[,1], 0.975),
+                        BlomEstimator(sd25Dist[,1], 0.975))) %>%
+  mutate(pop = c("Reference", "GA 1", "GA 2", "GA 3", "SD 1", "SD 2", "SD 3")) %>%
+  rename("2.5" = "X2.5", 
+         "97.5" = "X97.5")
+
 intVals %>%
   ggplot(aes(x = pop, y = mean)) +
   geom_point() +
@@ -305,11 +278,12 @@ intVals %>%
   geom_hline(yintercept = intVals %>% filter(pop == "Reference") %>% .$`2.5`, 
              linetype = 2, size = 0.1) +
   geom_hline(yintercept = intVals %>% filter(pop == "Reference") %>% .$`97.5`, 
-             linetype = 2, size = 0.1)
+             linetype = 2, size = 0.1) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))
 
 
-
-#Combine the slope and intercept values into one table
+# Combine the slope and intercept values into one table
 regVals <- 
   bind_cols(intVals %>%
           rename(`int2.5` = `2.5`, 
@@ -323,19 +297,20 @@ regVals <-
           select(s2.5, smean, s97.5, pop))
 
 #Write tables to file
-
 regVals %>%
   write.csv(paste0("output/", Sys.Date(), "_regVals.csv"))
-wae.pred.values %>%
-  write.csv(paste0("output/", Sys.Date(), "_wae.pred.values.csv"))
 
 # Do the 95% estimates of slope for each population overlap the slope value for the reference population?
-regVals %>%
+
+regVals <- 
+  regVals %>%
   mutate(row_num = row_number(),
          pop = row.names(regVals),
          is_slope_overlap = ifelse(smean > (regVals %>% filter(pop == "Reference") %>% .$s2.5) &
                                smean < (regVals %>% filter(pop == "Reference") %>% .$s97.5), TRUE, FALSE))
-  
+
+
+#-----------------------------------------------------------  
 # CONVERT BELOW TO A GGPLOT WITH H AND V ERRBARS
 
 #Plot all slope and intercept values on the same plot?  With horizontal and 
@@ -359,18 +334,19 @@ plotCI(regVals[,5], regVals[,2], uiw = 0.5, ui=regVals[,6], li=regVals[,4], err=
 legend("topright", pch=c(8,0,2,5,15,17,18), legend=c("Reference", "GA 1", "GA 2", 
                                                      "GA 3", "SD 1", "SD 2", "SD 3"),
        bty="n")
+
+#-----------------------------------------------------------  
+
+
        
-#IF REFERENCE POPULATION SLOPE AND INTERCEPT 95% CI DO NOT OVERLAP WITH POPULATION
-#LEVEL SLOPE AND INTERCEPT, DETERMINE WHETHER THE INTERVAL AROUND THE DIFFERENCE
-#IN SLOPES CONTAINS ZERO; FROM POPE AND KRUSE 2007 AIFFD, page 433
-
-(mean(refDist[,2])-mean(sd25Dist[,2]))+(1.96*sqrt(((sd(refDist[,2])/(sqrt(nrow(refDist))))^2+(sd(refDist[,2])/(sqrt(nrow(refDist))))^2)))
-(mean(refDist[,2])-mean(sd25Dist[,2]))-(1.96*sqrt(((sd(refDist[,2])/(sqrt(nrow(refDist))))^2+(sd(refDist[,2])/(sqrt(nrow(refDist))))^2)))
-
-
+# IF REFERENCE POPULATION SLOPE AND INTERCEPT 95% CI DO NOT OVERLAP WITH POPULATION
+# LEVEL SLOPE AND INTERCEPT, DETERMINE WHETHER THE INTERVAL AROUND THE DIFFERENCE
+# IN SLOPES CONTAINS ZERO; FROM POPE AND KRUSE 2007 AIFFD, page 433 (i.e., rows 
+# that are FALSE in regVals$is_slope_overlap)
 
 
 #Fx to return the interval around the differences in slopes
+
 interval_around_differences <- function(population, reference){
   
   upper <- (mean(reference$slope) - mean(population$slope)) + (1.96*sqrt(std_error(reference$slope)^2 + std_error(population$slope)^2))
@@ -384,6 +360,6 @@ interval_around_differences <- function(population, reference){
 list(ga2Dist, ga3Dist, ga4Dist, 
   sd4Dist, sd13Dist, sd25Dist) %>%
   map_dfr(interval_around_differences, refDist) %>%
-  mutate(pop = c("GA1", "GA2", "GA3", "SD1", "SD2", "SD3"))
+  mutate(pop = c("GA 1", "GA 2", "GA 3", "SD 1", "SD 2", "SD 3"))
 
 
