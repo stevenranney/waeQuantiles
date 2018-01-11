7539a26d8c8cbbcfc269563affa7429c81da7114
Steven H. Ranney <steven.ranney@gmail.com>
2018-01-10 22:18:58 -0700

    Updating code and manuscript.

---
+++ code/helper_functions.R
@@ -0,0 +1,44 @@
+
+
+## BLOM ESTIMATOR
+
+#Calculate the Blom estimator for Q3 values for all length categories
+BlomEstimator <- function(vec, percentile){
+    #The Blom method here is as described from Gerow 2009
+    vec <- sort(vec)
+    #Identify which value in vec is at the percentile requested
+    nDf <- percentile*(length(vec)+1)
+    #round the number down to the nearest integer
+    L <- floor(nDf)
+    #Calculate difference
+    s <- (nDf-L)
+    #Calculate the percentile value
+    return(vec[L]+(s*(vec[L+1]-vec[L])))
+
+    }
+
+
+## BOOTSTRAP ESTIMATES OF SLOPE AND INTERCEPT
+
+library(quantreg)
+library(dplyr)
+
+EstLinQuantRegCoefDist <- function(data, n, numSamples, tau = 0.75){
+  
+    regCoefs <- 
+      as.data.frame(matrix(NA, nrow = n, ncol=2)) %>%
+      rename(intercept = V1, 
+             slope = V2)      
+  
+    for(i in 1:n){
+      rowVec <- sample(1:nrow(data), numSamples, replace=T)
+      tmp <- data[rowVec, ]
+      mod <- rq(log10(weight)~log10(length), data=tmp, tau = tau)
+    
+      regCoefs[i,1] <- mod$coefficients[1]
+      regCoefs[i,2] <- mod$coefficients[2]
+    }
+    
+    names(regCoefs) <- c("intercept", "slope")
+    return(regCoefs)
+  }
